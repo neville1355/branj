df7a80c78183af8f29b148300d6fb59ecd78412b
Jordan <jordann@nulogy.com>
2026-01-05 13:13:32 -0500

    Update branj.rb SHA... again

--- Formula/branj.rb
+++ Formula/branj.rb
@@ -3,7 +3,7 @@ class Branj < Formula
   homepage "https://github.com/neville1355/branj"
   url "https://github.com/neville1355/branj/archive/refs/heads/master.zip"
   version "1.0.0"
-  sha256 "920f042c12a90da45ffbc5137bf64a3823cf3368606114f801d6724ae6a495d0"
+  sha256 "b64611eed37cbe1ec2acea6053dfbec0d51a6ec5c1abda8ec57768466e65f6d0"
 
   def install
     bin.install "branj"
