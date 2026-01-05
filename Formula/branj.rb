fff54f953073a179fcce40fe1d64b009ab5fcdaf
Jordan Neville <jordan.neville@odaia.ai>
2026-01-05 14:00:47 -0500

    remove sha

--- Formula/branj.rb
+++ Formula/branj.rb
@@ -2,8 +2,7 @@ class Branj < Formula
   desc "Automatically check out a git branch corresponding to the name of a given JIRA ticket"
   homepage "https://github.com/neville1355/branj"
   url "https://github.com/neville1355/branj/archive/refs/heads/master.zip"
-  version "1.0.2"
-  sha256 :no_check
+  version "1.0.3"
 
   def install
     bin.install "branj"
